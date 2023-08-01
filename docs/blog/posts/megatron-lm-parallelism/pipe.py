3fc62a266d3fee006890925fa07a02af65326203
xrsrke <b3f0cus@icloud.com>
2023-08-01 14:21:22 +0700

    fixed forward and backward of pipeline

--- docs/blog/posts/megatron-lm-parallelism/pipe.py
+++ docs/blog/posts/megatron-lm-parallelism/pipe.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from contextlib import contextmanager
 from queue import Queue
 from threading import Thread
+import time
 
 import torch
 from torch import nn
@@ -26,6 +27,7 @@ def wait_and_execute(device: torch.device, in_queue: Queue, out_queue: Queue):
         try:
             output = task.compute()
         except Exception:
+            raise RuntimeError(f"Failed to execute a task on {device}")
             out_queue.put(QueueOutput(task=task, output=None, is_done=False))
             continue
 
@@ -88,13 +90,13 @@ class DetermisticScheduler:
         ), "The number of partitions must be \
             greater than 0"
 
-        self.n_partitions = n_partitions
-        self.n_microbatches = n_microbatches
-        n_clock_cycles = self.n_partitions + self.n_microbatches - 1
+        n_partitions = n_partitions
+        n_microbatches = n_microbatches
+        n_clock_cycles = n_partitions + n_microbatches - 1
 
         for clock_idx in range(n_clock_cycles):
-            start_partrition = max(clock_idx + 1 - self.n_microbatches, 0)
-            end_partition = min(clock_idx + 1, self.n_partitions)
+            start_partrition = max(clock_idx + 1 - n_microbatches, 0)
+            end_partition = min(clock_idx + 1, n_partitions)
 
             tasks = []
             for partition_idx in range(start_partrition, end_partition):
@@ -105,7 +107,7 @@ class DetermisticScheduler:
 
 
 class Task:
-    def __init__(self, compute: Callable[[], torch.Tensor], is_done: bool):
+    def __init__(self, compute: Callable[[], torch.Tensor], is_done: bool = False):
         self._compute = compute
         self.is_done = is_done
 
@@ -147,12 +149,12 @@ class Pipeline:
 
         with spawn_worker(devices) as (in_queues, out_queues):
             for schedule in scheduler.generate(n_batches, n_partitions):
-                # self._depend(schedule)
+                # _depend(schedule)
                 self._compute(schedule, in_queues, out_queues)
 
     # def _depend(self, schedule: List[Tuple[int, int]]):
     #     """Enforce the dependency between batches and partitions."""
-    #     batches = self.batches
+    #     batches = batches
 
     #     for microbatch_idx, partition_idx in schedule:
     #         if microbatch_idx != 0:
@@ -182,3 +184,101 @@ class Pipeline:
 
             # put the output back to the batch
             batches[microbatch_idx] = output
+
+
+def create_non_parallel_model(partitions):
+    non_parallel_model = nn.Sequential(*[AddOne(partition_idx=x, is_logging=False) for x in range(N_PARTITIONS)])
+    for non_parallel_layer, original_partition in zip(non_parallel_model, partitions):
+        non_parallel_layer.load_state_dict(original_partition[0].state_dict())
+    return non_parallel_model
+
+
+def create_non_parallel_batch(batch):
+    non_parallel_batch = batch.detach().clone()
+    non_parallel_batch.grad = None
+    return non_parallel_batch
+
+
+if __name__ == "__main__":
+    N_MICROBATCHES = 3
+    N_PARTITIONS = 2
+
+    forward_timeline = []
+    backward_timeline = []
+
+    def backward_hook(module, grad_input, grad_output):
+        backward_timeline.append((module.microbatch_idx - 1, module.partition_idx))
+        module.microbatch_idx -= 1
+
+    class AddOne(nn.Module):
+        def __init__(self, partition_idx, is_logging):
+            super().__init__()
+            self.microbatch_idx = 0
+            self.partition_idx = partition_idx
+            self.is_logging = is_logging
+            self.net = nn.Linear(1, 1)
+            self.register_backward_hook(backward_hook)
+
+        def forward(self, x):
+            if self.is_logging:
+                time.sleep(0.5)
+                forward_timeline.append((self.microbatch_idx, self.partition_idx))
+                self.microbatch_idx += 1
+
+            return self.net(x)
+
+    forward_timeline = forward_timeline
+    backward_timeline = backward_timeline
+    batch = torch.arange(0, N_MICROBATCHES, dtype=torch.float32, requires_grad=True)
+    microbatches = [x.unsqueeze(0) for x in batch.unbind()]
+    partitions = [nn.Sequential(AddOne(partition_idx=x, is_logging=True)) for x in range(N_PARTITIONS)]
+    devices = [torch.device("cpu") for _ in range(N_PARTITIONS)]
+    non_parallel_model = create_non_parallel_model(partitions)
+    non_parallel_batch = create_non_parallel_batch(batch)
+
+    results = {}
+
+    def loss_func(x):
+        return x.mean()
+
+    pipeline = Pipeline(microbatches, partitions, devices)
+
+    assert pipeline.batches == microbatches
+    assert pipeline.partitions == partitions
+
+    pipeline.fit()
+
+    assert forward_timeline == [(0, 0), (1, 0), (0, 1), (2, 0), (1, 1), (2, 1)]
+
+    outputs = microbatches
+    non_parallel_outputs = [non_parallel_model(x.unsqueeze(0)) for x in non_parallel_batch.unbind()]
+
+    for x, y in zip(outputs, non_parallel_outputs):
+        assert torch.allclose(x, y)
+
+    for x in outputs:
+        loss = loss_func(x)
+        loss.backward()
+
+    assert backward_timeline == [(2, 1), (2, 0), (1, 1), (1, 0), (0, 1), (0, 0)] or backward_timeline == [
+        (2, 1),
+        (2, 0),
+        (1, 1),
+        (0, 1),
+        (1, 0),
+        (0, 0),
+    ]
+
+    for x in non_parallel_outputs:
+        loss = loss_func(x)
+        loss.backward()
+
+    assert batch.grad is not None
+
+    for partition in partitions:
+        for param in partition.parameters():
+            assert param.grad is not None
+
+    for partition_idx in range(N_PARTITIONS):
+        for w1, w2 in zip(partitions[partition_idx].parameters(), non_parallel_model[partition_idx].parameters()):
+            assert torch.allclose(w1.grad, w2.grad)
