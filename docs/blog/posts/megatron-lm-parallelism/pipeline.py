3aba7f71c3ac6d6dea07434a360b88052000f12d
xrsrke <b3f0cus@icloud.com>
2023-08-04 14:19:26 +0700

    refactor scheduler

--- docs/blog/posts/megatron-lm-parallelism/pipeline.py
+++ docs/blog/posts/megatron-lm-parallelism/pipeline.py
@@ -75,44 +75,6 @@ def spawn_worker(
     yield (in_queues, out_queues)
 
 
-# class DetermisticScheduler:
-#     """
-#     torchgpipe: On-the-fly Pipeline Parallelism for Training Giant Models
-#     https://arxiv.org/abs/2004.09910
-
-#     Section 3.2.1: Forward Dependency: Deterministic Clock-cycle
-#     """
-
-#     def generate(
-#         self,
-#         n_microbatches: int,
-#         n_partitions: int,
-#     ) -> Iterable[List[Tuple[Annotated[int, "batch_idx"], Annotated[int, "partition_idx"]]]]:
-#         assert (
-#             n_microbatches > 0
-#         ), "The number of microbatches must be \
-#             greater than 0"
-#         assert (
-#             n_partitions > 0
-#         ), "The number of partitions must be \
-#             greater than 0"
-
-#         n_partitions = n_partitions
-#         n_microbatches = n_microbatches
-#         n_clock_cycles = n_partitions + n_microbatches - 1
-
-#         for clock_idx in range(n_clock_cycles):
-#             start_partrition = max(clock_idx + 1 - n_microbatches, 0)
-#             end_partition = min(clock_idx + 1, n_partitions)
-
-#             tasks = []
-#             for partition_idx in range(start_partrition, end_partition):
-#                 microbatch_idx = clock_idx - partition_idx
-#                 tasks.append((microbatch_idx, partition_idx))
-
-#             yield tasks
-
-
 def clock_cycles(n_microbatches, n_partritions):
     n_clock_cycles = n_partritions + n_microbatches - 1
     for clock_idx in range(n_clock_cycles):
